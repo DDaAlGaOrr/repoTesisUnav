2a4060b2d58f0b02637c4c761f28a248aab1c276
DDaAlGaOrr <daalgaor.17@gmail.com>
2022-03-03 16:56:58 -0700

    problem in connection to mysql solved

--- dbConection.js
+++ dbConection.js
@@ -3,14 +3,14 @@ const mysql = require('mysql')
 const cnx = mysql.createConnection({
     host:'localhost',
     user:'root',
-    password:'',
-    database:'mydb'
+    password:'root',
+    database:'biblio_tesis'
     // database:'sakila'
 })
 /* --------------------------------------------------------------------------------------------------------- */
 cnx.connect((error)=>{
     if(error){
-        console.log('error en la conexion')
+        console.log('error en la conexion' + error)
         return
     }
     else{
